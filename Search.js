106e8b544e62be1327abada04e1334667eef906e
Juliana G P S Ferreira <julianaferry@users.noreply.github.com>
2018-07-16 15:17:44 -0300

    Add files via upload

---
+++ Search.js
@@ -0,0 +1,85 @@
+import React, {Component} from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+import { Debounce } from "react-throttle";
+import Book from "./Book";
+import * as BooksAPI from "./BooksAPI";
+
+class Search extends Component{
+    constructor(props) {
+        super(props);
+        this.updateQuery = this.updateQuery.bind(this);
+    }
+    static propTypes = {
+        books: PropTypes.array.isRequired,
+        changeShelf: PropTypes.func.isRequired
+    };
+    
+    state = {
+        books: [],
+        searchBooks: []
+    };
+   
+    componentWillUnmount(){
+        this.props.updateQuery('');
+    }
+
+
+    updateQuery = (query) => {
+       
+            BooksAPI.search(query).then((books) => {
+               
+              const array = [{books}];
+              
+                    array.forEach((book, index) => {
+                        let myBook = this.props.books.find((b) => b.id === book.id);
+                        book.shelf = myBook ? myBook.shelf : 'none';
+                        books[index] = book;
+                    });
+
+                    this.setState({
+                        searchBooks: books
+                    });
+                
+
+            });
+            
+            this.setState({
+                searchBooks: []
+            });
+        
+    };
+
+    render(){
+      
+        return(
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link className="close-search" to="/">Close</Link>
+                    <div className="search-books-input-wrapper">
+                        <Debounce time="800" handler="onChange">
+                            <input
+                                type="text"
+                                placeholder="Search by title or author"
+                                onChange={(event) => this.updateQuery(event.target.value)}
+                            />
+                        </Debounce>
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <ol className="books-grid">
+                        {this.props.books && this.props.books.map((book) => (
+                            <li key={book.id} className="contact-list-item">
+                                <Book
+                                    book={book}
+                                    changeShelf={this.props.changeShelf} />
+                            </li>
+                        ))}
+                    </ol>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Search;
